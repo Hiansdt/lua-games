51d293df6024a759c58d9fcfc1a29d2c0cafc2bb
Hiansdt <vinijao123690@gmail.com>
2023-06-05 11:06:07 -0300

    Animações de ataque e combo adicionadas - jogo 2

--- platformer-2/main.lua
+++ platformer-2/main.lua
@@ -5,7 +5,7 @@ local curQuad = nil
 local walkSpriteTime = 0.075
 local idleSpriteTime = 0.2
 local jumpSpriteTime = 0.05
-local attackSpriteTime = 0.05
+local attackSpriteTime = 0.1
 
 local walkSpriteTimer = 0
 local idleSpriteTimer = 0
@@ -13,6 +13,12 @@ local jumpSpriteTimer = 0
 local attackSpriteTimer = 0
 
 local attackSpriteAnimationTimer = 0
+local idleSpriteAnimationTimer = 0
+
+local notAttackingTimer = 0
+local totalAttacks = 0
+local currentAttack = 1
+local AttacksDone = 0
 
 function love.load()
     player.x = 0
@@ -22,9 +28,10 @@ function love.load()
     player.vx = 0
     player.vy = 0
     player.jumps = 0
+    player.attacks = 0
     player.isIdle = true
     player.isJumping = false
-    player.isAttacking = false
+    player.isAttacking4 = false
     player.isWalking = false
     floor.x = 0
     floor.y = 500
@@ -64,18 +71,36 @@ function love.load()
     quad7Jump = love.graphics.newQuad(quadWidth * 6, 0, quadWidth, quadHeight, jumpSpriteSheet:getDimensions())
     quad8Jump = love.graphics.newQuad(quadWidth * 7, 0, quadWidth, quadHeight, jumpSpriteSheet:getDimensions())
 
-    attackingSpriteSheet = love.graphics.newImage("Enchantress/Attack_4.png")
+    attackingSpriteSheet4 = love.graphics.newImage("Enchantress/Attack_4.png")
+    attackingSpriteSheet3 = love.graphics.newImage("Enchantress/Attack_3.png")
+    attackingSpriteSheet2 = love.graphics.newImage("Enchantress/Attack_2.png")
+    attackingSpriteSheet1 = love.graphics.newImage("Enchantress/Attack_1.png")
 
-    quad1Attacking = love.graphics.newQuad(0, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad2Attacking = love.graphics.newQuad(quadWidth, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad3Attacking = love.graphics.newQuad(quadWidth * 2, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad4Attacking = love.graphics.newQuad(quadWidth * 3, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad5Attacking = love.graphics.newQuad(quadWidth * 4, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad6Attacking = love.graphics.newQuad(quadWidth * 5, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad7Attacking = love.graphics.newQuad(quadWidth * 6, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad8Attacking = love.graphics.newQuad(quadWidth * 7, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad9Attacking = love.graphics.newQuad(quadWidth * 8, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
-    quad10Attacking = love.graphics.newQuad(quadWidth * 9, 0, quadWidth, quadHeight, attackingSpriteSheet:getDimensions())
+    quad1Attacking4 = love.graphics.newQuad(0, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad2Attacking4 = love.graphics.newQuad(quadWidth, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad3Attacking4 = love.graphics.newQuad(quadWidth * 2, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad4Attacking4 = love.graphics.newQuad(quadWidth * 3, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad5Attacking4 = love.graphics.newQuad(quadWidth * 4, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad6Attacking4 = love.graphics.newQuad(quadWidth * 5, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad7Attacking4 = love.graphics.newQuad(quadWidth * 6, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad8Attacking4 = love.graphics.newQuad(quadWidth * 7, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad9Attacking4 = love.graphics.newQuad(quadWidth * 8, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+    quad10Attacking4 = love.graphics.newQuad(quadWidth * 9, 0, quadWidth, quadHeight, attackingSpriteSheet4:getDimensions())
+
+    quad1Attacking3 = love.graphics.newQuad(0, 0, quadWidth, quadHeight, attackingSpriteSheet3:getDimensions())
+    quad2Attacking3 = love.graphics.newQuad(quadWidth, 0, quadWidth, quadHeight, attackingSpriteSheet3:getDimensions())
+    quad3Attacking3 = love.graphics.newQuad(quadWidth * 2, 0, quadWidth, quadHeight, attackingSpriteSheet3:getDimensions())
+
+    quad1Attacking2 = love.graphics.newQuad(0, 0, quadWidth, quadHeight, attackingSpriteSheet2:getDimensions())
+    quad2Attacking2 = love.graphics.newQuad(quadWidth, 0, quadWidth, quadHeight, attackingSpriteSheet2:getDimensions())
+    quad3Attacking2 = love.graphics.newQuad(quadWidth * 2, 0, quadWidth, quadHeight, attackingSpriteSheet2:getDimensions())
+
+    quad1Attacking1 = love.graphics.newQuad(0, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
+    quad2Attacking1 = love.graphics.newQuad(quadWidth, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
+    quad3Attacking1 = love.graphics.newQuad(quadWidth * 2, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
+    quad4Attacking1 = love.graphics.newQuad(quadWidth * 3, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
+    quad5Attacking1 = love.graphics.newQuad(quadWidth * 4, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
+    quad6Attacking1 = love.graphics.newQuad(quadWidth * 5, 0, quadWidth, quadHeight, attackingSpriteSheet1:getDimensions())
 
     curQuad = quad1Idle
 end
@@ -92,15 +117,19 @@ function love.keypressed(key)
         player.isAttacking = false
         player.isWalking = true
     elseif key == "down" then
-        curQuad = quad1Attacking
+        if currentAttack == 1 then
+            curQuad = quad1Attacking1
+        totalAttacks = totalAttacks + 1
+        player.attacks = player.attacks + 1
         player.isIdle = false
         player.isAttacking = true
+        end
     end
 end
 
 function love.update(dt)
 
-    if player.isIdle then
+    if player.isIdle and idleSpriteAnimationTimer > 5 then
         idleSpriteTimer = idleSpriteTimer + dt
         if idleSpriteTimer > idleSpriteTime then
             if curQuad == quad1Idle then
@@ -125,6 +154,11 @@ function love.update(dt)
         player.x = player.x + player.vx * dt
         walkSpriteTimer = walkSpriteTimer + dt
         player.isIdle = false
+        player.isAttacking = false
+        currentAttack = 1
+        AttacksDone = 0
+        totalAttacks = 0
+        player.isWalking = true
         if walkSpriteTimer > walkSpriteTime and not player.isIdle then
             if curQuad == quad1Walk then
                 curQuad = quad2Walk
@@ -152,6 +186,11 @@ function love.update(dt)
         player.x = player.x + player.vx * dt
         player.vx = 0
         player.isIdle = false
+        player.isAttacking = false
+        currentAttack = 1
+        AttacksDone = 0
+        totalAttacks = 0
+        player.isWalking = true
         walkSpriteTimer = walkSpriteTimer + dt
         if walkSpriteTimer > walkSpriteTime and not player.isIdle then
             if curQuad == quad1Walk then
@@ -202,41 +241,109 @@ function love.update(dt)
     if player.isIdle == false and not love.keyboard.isDown("right") and not love.keyboard.isDown("left") and not player.isJumping and not player.isAttacking then
         player.isIdle = true
         curQuad = quad1Idle
+        idleSpriteAnimationTimer = idleSpriteAnimationTimer + dt
     end
 
     if player.isAttacking then
         attackSpriteAnimationTimer = attackSpriteAnimationTimer + dt
         attackSpriteTimer = attackSpriteTimer + dt
         if attackSpriteTimer > attackSpriteTime then
-            if curQuad == quad1Attacking then
-                curQuad = quad2Attacking
-            elseif curQuad == quad2Attacking then
-                curQuad = quad3Attacking
-            elseif curQuad == quad3Attacking then
-                curQuad = quad4Attacking
-            elseif curQuad == quad4Attacking then
-                curQuad = quad5Attacking
-            elseif curQuad == quad5Attacking then
-                curQuad = quad6Attacking
-            elseif curQuad == quad6Attacking then
-                curQuad = quad7Attacking
-            elseif curQuad == quad7Attacking then
-                curQuad = quad8Attacking
-            elseif curQuad == quad8Attacking then
-                curQuad = quad9Attacking
-            elseif curQuad == quad9Attacking then
-                curQuad = quad10Attacking
-            elseif curQuad == quad10Attacking then
-                curQuad = quad1Idle
-            end
-            if attackSpriteAnimationTimer > attackSpriteTime * 10 then
+            if curQuad == quad1Attacking1 then
+                currentAttack = 1
+                curQuad = quad2Attacking1
+            elseif curQuad == quad2Attacking1 then
+                curQuad = quad3Attacking1
+            elseif curQuad == quad3Attacking1 then
+                curQuad = quad4Attacking1
+            elseif curQuad == quad4Attacking1 then
+                curQuad = quad5Attacking1
+            elseif curQuad == quad5Attacking1 then
+                curQuad = quad6Attacking1
+                AttacksDone = AttacksDone + 1
+            elseif curQuad == quad6Attacking1 and totalAttacks == 1 then
                 player.isAttacking = false
                 player.isIdle = true
-                attackSpriteAnimationTimer = 0
                 curQuad = quad1Idle
+                totalAttacks = 0
+                currentAttack = 1
+                AttacksDone = 0
+            elseif curQuad == quad6Attacking1 and totalAttacks > 1 then
+                currentAttack = 2
+                curQuad = quad1Attacking2
+            elseif curQuad == quad1Attacking2 then
+                curQuad = quad2Attacking2
+            elseif curQuad == quad2Attacking2 then
+                curQuad = quad3Attacking2
+                AttacksDone = AttacksDone + 1
+            elseif curQuad == quad3Attacking2 and totalAttacks <= 2 then
+                player.isAttacking = false
+                player.isIdle = true
+                curQuad = quad1Idle
+                totalAttacks = 0
+                currentAttack = 1
+                AttacksDone = 0
+            elseif curQuad == quad3Attacking2 and totalAttacks > 2 then
+                currentAttack = 3
+                curQuad = quad1Attacking3
+            elseif curQuad == quad1Attacking3 then
+                curQuad = quad2Attacking3
+            elseif curQuad == quad2Attacking3 then
+                curQuad = quad3Attacking3
+                AttacksDone = AttacksDone + 1
+            elseif curQuad == quad3Attacking3 and totalAttacks <= 3 then
+                player.isAttacking = false
+                player.isIdle = true
+                curQuad = quad1Idle
+                totalAttacks = 0
+                currentAttack = 1
+                AttacksDone = 0
+            elseif curQuad == quad3Attacking3 and totalAttacks > 3 then
+                currentAttack = 4
+                curQuad = quad1Attacking4
+            elseif curQuad == quad1Attacking4 then
+                curQuad = quad2Attacking4
+            elseif curQuad == quad2Attacking4 then
+                curQuad = quad3Attacking4
+            elseif curQuad == quad3Attacking4 then
+                curQuad = quad4Attacking4
+            elseif curQuad == quad4Attacking4 then
+                curQuad = quad5Attacking4
+            elseif curQuad == quad5Attacking4 then
+                curQuad = quad6Attacking4
+            elseif curQuad == quad6Attacking4 then
+                curQuad = quad7Attacking4
+            elseif curQuad == quad7Attacking4 then
+                curQuad = quad8Attacking4
+            elseif curQuad == quad8Attacking4 then
+                curQuad = quad9Attacking4
+            elseif curQuad == quad9Attacking4 then
+                curQuad = quad10Attacking4
+                AttacksDone = AttacksDone + 1
+            elseif curQuad == quad10Attacking4 and totalAttacks <= 4 then
+                player.isAttacking = false
+                player.isIdle = true
+                curQuad = quad1Idle
+                totalAttacks = 0
+                currentAttack = 1
+                AttacksDone = 0
+            else
+                currentAttack = 1
+                curQuad = quad1Attacking1
+            end
+            if AttacksDone == totalAttacks then
+                player.isAttacking = false
+                player.isIdle = true
+                curQuad = quad1Idle
+                totalAttacks = 0
+                currentAttack = 1
             end
             attackSpriteTimer = attackSpriteTimer - attackSpriteTime
         end
+    end
+
+    if player.isAttacking == false then
+        notAttackingTimer = notAttackingTimer + dt
+    else notAttackingTimer = 0
     end
 
     player.vy = player.vy + gravity * dt
@@ -273,7 +380,15 @@ function love.draw()
     elseif player.isJumping then
         love.graphics.draw(jumpSpriteSheet, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
     elseif player.isAttacking then
-        love.graphics.draw(attackingSpriteSheet, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
+        if currentAttack == 1 then
+            love.graphics.draw(attackingSpriteSheet1, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
+        elseif currentAttack == 2 then
+            love.graphics.draw(attackingSpriteSheet2, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
+        elseif currentAttack == 3 then
+            love.graphics.draw(attackingSpriteSheet3, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
+        elseif currentAttack == 4 then
+            love.graphics.draw(attackingSpriteSheet4, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
+        end
     elseif player.isWalking then
         love.graphics.draw(walkingSpriteSheet, curQuad, player.x, player.y, 0, 1, 1, 0, 0)
     end
